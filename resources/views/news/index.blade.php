813c3f1efd63703640dc90cd353df5faed6123f9
Haakon Kverneng <haakon@haakon.local>
2017-07-06 16:59:34 +0200

    Minor HTML fix to news holder

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -9,18 +9,16 @@
         <div class="panel-body">
             @foreach ($news as $new)
                 <div class="row">
-                    <div class="col-md-12">
-                        <div class="col-md-8">
-                            <a href="/news/{{ $new->id }}"><h4>{{ $new->title }}</h4></a>
-                            <small>Publisert: {{ $new->created_at->toFormattedDateString() }}, Skrevet av: {{ $new->user->name }}</small><br>
-                            <b>{{ $new->intro }}</b><br><br>
-                            <a href="/news/{{ $new->id }}"><span class="glyphicon glyphicon-info-sign"></span> Les mer</a>
-                        </div>
-                        <div class="col-md-4">
-                            <a href="/news/{{ $new->id }}">
-                                <img class="img-responsive" src="{{ $new->image }}" />
-                            </a>
-                        </div>
+                    <div class="col-md-8">
+                        <a href="/news/{{ $new->id }}"><h4>{{ $new->title }}</h4></a>
+                        <small>Publisert: {{ $new->created_at->toFormattedDateString() }}, Skrevet av: {{ $new->user->name }}</small><br>
+                        <b>{{ $new->intro }}</b><br><br>
+                        <a href="/news/{{ $new->id }}"><span class="glyphicon glyphicon-info-sign"></span> Les mer</a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="/news/{{ $new->id }}">
+                            <img class="img-responsive" src="{{ $new->image }}" />
+                        </a>
                     </div>
                 </div>
                 <hr>
