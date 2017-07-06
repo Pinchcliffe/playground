0ff7b4a305d962a2195c724d6c499101508fbe4f
Pinchcliffe <haakonkverneng@gmail.com>
2017-07-06 18:48:42 +0200

    Fixed issue with sidebar when logged out

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,25 +1,36 @@
 <div>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            Nyhetsarkiv
-        </div>
-        <div class="panel-body">
-            <ol class="list-unstyled">
-                @foreach ($archives as $archive)
-
-                    <li><a href="/news?month={{ $archive['month'] }}&year={{ $archive['year'] }}">{{ $archive['month'] }} {{ $archive['year'] }}</a></li>
-
-                @endforeach
-            </ol>
-        </div>
-    </div>
-
     @if (Auth::user())
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Nyhetsarkiv
+            </div>
+            <div class="panel-body">
+                <ol class="list-unstyled">
+                    @foreach ($archives as $archive)
+
+                        <li><a href="/news?month={{ $archive['month'] }}&year={{ $archive['year'] }}">{{ $archive['month'] }} {{ $archive['year'] }}</a></li>
+
+                    @endforeach
+                </ol>
+            </div>
+        </div>
+
         <a href="/news/create">
             <button class="btn btn-success btn-sm"><span class="glyphicon glyphicon-pencil"></span> Skriv en nyhet</button>
         </a>
         <a href="/news">
             <button class="btn btn-primary btn-sm">Vis alle nyheter</button>
         </a>
+    @endif
+
+    @if (Auth::guest())
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Laravel 5.4
+            </div>
+            <div class="panel-body">
+                <img class="img-circle" src="{{ asset('images/laravel.svg') }}" height="100" width="100" />
+            </div>
+        </div>
     @endif
 </div>
